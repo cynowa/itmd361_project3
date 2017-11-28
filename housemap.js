db84f308823b36f5cca3653687242c59501d36f4
cynowa <31426990+cynowa@users.noreply.github.com>
2017-11-28 00:22:55 -0600

    added markers for other points of interest on map

--- housemap.js
+++ housemap.js
@@ -10,6 +10,31 @@ function initMap() {
     map: map,
     title: '417 N Grant, Westmont'});
 
+  var marker = new google.maps.Marker({
+    position: {lat: 41.795456, lng: -87.976445},
+    map: map,
+    title: 'Train Station'});
+
+  var marker = new google.maps.Marker({
+    position: {lat:  41.806219, lng: -87.976167},
+    map: map,
+    title: 'Library'});
+
+  var marker = new google.maps.Marker({
+    position: {lat: 41.806830, lng: -87.975819},
+    map: map,
+    title: 'Bus Stop'});
+
+  var marker = new google.maps.Marker({
+    position: {lat: 41.811172, lng: -87.973538},
+    map: map,
+    title: 'Jewel'});
+
+  var marker = new google.maps.Marker({
+    position: {lat: 41.811099, lng: -87.989021},
+    map: map,
+    title: 'Trader Joe\'s'});
+
   var circle = new google.maps.Circle({
     strokeColor: '#FF0000',
     strokeOpacity: 0.8,
